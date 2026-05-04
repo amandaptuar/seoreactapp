6e84f7154797c7e94d68a4a4a140b18bd7e0bbff
amandaptuar <amandaptuar@gmail.com>
2026-05-04 15:40:52 +0530

    removed our team section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
-import Team from '../components/Team';
 import Services from '../components/Services';
 import Reviews from '../components/Reviews';
 import FAQ from '../components/FAQ';
@@ -16,7 +15,6 @@ const Home = () => {
       <Header />
       <Hero />
       <About />
-      <Team />
       <Services />
       <Reviews />
       <FAQ />
